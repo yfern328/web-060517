2e5c2c25b24d9d2b9353dd4ea5c847d65a83d3e9
Jeff Katz <jeffreyerickatz@gmail.com>
2017-06-06 11:59:21 -0400

    adding pigeon code

---
+++ nyc-pigeon-organizer-web-0616/README.md
@@ -0,0 +1,7 @@
+- focused on arrays, but did less looping
+- break things down into individual steps, and then have it
+- sandwich code is bad
+  became really particular about the array methods
+
+
+1. Breaking the problem down into steps
---
+++ nyc-pigeon-organizer-web-0616/nyc_pigeon_organizer.rb
@@ -0,0 +1,84 @@
+require 'pry-byebug'
+# pigeon_data = {
+#   :color => {
+#     :purple => ["Theo", "Peter Jr.", "Lucky"],
+#     :grey => ["Theo", "Peter Jr.", "Ms. K"],
+#     :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
+#     :brown => ["Queenie", "Alex"]
+#   },
+#   :gender => {
+#     :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
+#     :female => ["Queenie", "Ms. K"]
+#   },
+#   :lives => {
+#     "Subway" => ["Theo", "Queenie"],
+#     "Central Park" => ["Alex", "Ms. K", "Lucky"],
+#     "Library" => ["Peter Jr."],
+#     "City Hall" => ["Andrew"]
+#   }
+# }
+
+def nyc_pigeon_organizer(data)
+
+  binding.pry
+  # make our data structure
+
+    # get all the names
+    names = data.values.map do |hash|
+      hash.values
+    end.flatten.uniq
+
+
+    # each name is the key, and the value of that name is an empty hash
+    hash = {}
+    # go through each name
+    # add each name into the hash
+    # pigeon_list = {
+      # "theo" => {},
+      # "Peter Jr." => {},
+  # }
+  # each_with_object -> to remove sandwich code
+
+
+  # fill it in with the data
+  #
+end
+
+
+# pigeon_list = {
+#   "Theo" => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Subway"]
+#   },
+#   "Peter Jr." => {
+#     :color => ["purple", "grey"],
+#     :gender => ["male"],
+#     :lives => ["Library"]
+#   },
+#   "Lucky" => {
+#     :color => ["purple"],
+#     :gender => ["male"],
+#     :lives => ["Central Park"]
+#   },
+#   "Ms. K" => {
+#     :color => ["grey", "white"],
+#     :gender => ["female"],
+#     :lives => ["Central Park"]
+#   },
+#   "Queenie" => {
+#     :color => ["white", "brown"],
+#     :gender => ["female"],
+#     :lives => ["Subway"]
+#   },
+#   "Andrew" => {
+#     :color => ["white"],
+#     :gender => ["male"],
+#     :lives => ["City Hall"]
+#   },
+#   "Alex" => {
+#     :color => ["white", "brown"],
+#     :gender => ["male"],
+#     :lives => ["Central Park"]
+#   }
+# }
